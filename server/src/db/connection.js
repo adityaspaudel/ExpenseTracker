3c38ebef553695c5d5f91f016be66b370f082fb8
aaditya paudel <adityaspaudel@gmail.com>
2025-11-25 14:40:57 +0545

    index.js update

---
+++ server/src/db/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const dbConnect = async () => {
+  try {
+    const isConnected = await mongoose.connect(process.env.MONGODB_LOCAL_URI);
+    if (!isConnected) throw new Error("mongodb error");
+    console.log("connected to mongodb");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports=dbConnect
