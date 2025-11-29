ac0ce07bb80acfd29300aec718fe26ba43ed7eba
Aaditya Paudel <adityaspaudel@gmail.com>
2025-11-29 19:13:22 +0545

    middleware update

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config()
+dotenv.config();
 
-  // MONGODB_ATLAS_URI =
-		// 	"mongodb+srv://dbUser:dbUserPassword@cluster0.p9ldcdv.mongodb.net/?appName=Cluster0";
+// MONGODB_ATLAS_URI =
+// 	"mongodb+srv://dbUser:dbUserPassword@cluster0.p9ldcdv.mongodb.net/?appName=Cluster0";
+
+// MONGODB_LOCAL_URI = "mongodb://127.0.0.1:27017/expenseTracker";
 const dbConnect = async () => {
-  try {
-  
-    const isConnected = await mongoose.connect(process.env.MONGODB_ATLAS_URI);
-    if (!isConnected) throw new Error("mongodb error");
-    console.log(`connected to ${process.env.MONGODB_ATLAS_URI}`);
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const isConnected = await mongoose.connect(
+			process.env.MONGODB_ATLAS_URI || process.env.MONGODB_LOCAL_URI
+		);
+		if (!isConnected) throw new Error("mongodb error");
+		console.log(`connected to ${process.env.MONGODB_ATLAS_URI}`);
+	} catch (error) {
+		console.error(error);
+	}
 };
 
-module.exports=dbConnect
+module.exports = dbConnect;
--- server/src/middlewares/appMiddleware.js
+++ server/src/middlewares/appMiddleware.js
@@ -1,5 +1,5 @@
 const helmet = require("helmet");
-const xss = require("xss-clean");
+const { xss } = require("express-xss-sanitizer"); 
 const mongoSanitize = require("express-mongo-sanitize");
 const hpp = require("hpp");
 const cors = require("cors");
@@ -7,32 +7,32 @@ const rateLimit = require("express-rate-limit");
 
 // ---- Rate Limiter ----
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: "Too many requests, try again later",
-  standardHeaders: true,
-  legacyHeaders: false,
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	message: "Too many requests, try again later",
+	standardHeaders: true,
+	legacyHeaders: false,
 });
 
-// ---- CORS for Next.js ----
+// ---- CORS ----
 const corsMiddleware = cors({
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+	origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+	credentials: true,
+	methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 });
 
-// ---- Export all middlewares as array ----
+// ---- Middleware array ----
 const appMiddlewares = [
-  helmet({
-    contentSecurityPolicy: false,
-    crossOriginEmbedderPolicy: false,
-    crossOriginResourcePolicy: false,
-  }),
-  corsMiddleware,
-  xss(),
-  mongoSanitize(),
-  hpp(),
-  apiLimiter,
+	helmet({
+		contentSecurityPolicy: false,
+		crossOriginEmbedderPolicy: false,
+		crossOriginResourcePolicy: false,
+	}),
+	corsMiddleware,
+	xss(), 
+	mongoSanitize(),
+	hpp(),
+	apiLimiter,
 ];
 
 module.exports = appMiddlewares;
