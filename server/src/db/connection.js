2cb9c857f2d034f3281e794174d2835d0d9bf355
Aaditya Paudel <adityaspaudel@gmail.com>
2025-12-02 10:19:28 +0545

    bug fixed

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config()
+dotenv.config();
 
-  // MONGODB_ATLAS_URI =
-		// 	"mongodb+srv://dbUser:dbUserPassword@cluster0.p9ldcdv.mongodb.net/?appName=Cluster0";
+// PORT=8000 
+// MONGODB_ATLAS_URI="mongodb+srv://adityaspaudel_db_user:adityaspaudel_db_user@cluster0.kigzppt.mongodb.net/expenseTracker" 
+
+
+const MONGODB_LOCAL_URI = "mongodb://localhost:27017/expenseTracker";
 const dbConnect = async () => {
-  try {
-  
-    const isConnected = await mongoose.connect(process.env.MONGODB_ATLAS_URI);
-    if (!isConnected) throw new Error("mongodb error");
-    console.log(`connected to ${process.env.MONGODB_ATLAS_URI}`);
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const isConnected = await mongoose.connect(
+			`${process.env.MONGODB_ATLAS_URI}`
+		);
+		if (!isConnected) throw new Error("mongodb error");
+		else console.log(`connected to ${process.env.MONGODB_ATLAS_URI}`);
+	} catch (error) {
+		console.error("database connection error", error);
+	}
 };
 
-module.exports=dbConnect
+module.exports = dbConnect;
--- server/src/middlewares/appMiddleware.js
+++ server/src/middlewares/appMiddleware.js
@@ -7,32 +7,24 @@ const rateLimit = require("express-rate-limit");
 
 // ---- Rate Limiter ----
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: "Too many requests, try again later",
-  standardHeaders: true,
-  legacyHeaders: false,
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	message: "Too many requests, try again later",
+	standardHeaders: true,
+	legacyHeaders: false,
 });
 
 // ---- CORS for Next.js ----
-const corsMiddleware = cors({
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-});
+const corsMiddleware = cors();
 
 // ---- Export all middlewares as array ----
 const appMiddlewares = [
-  helmet({
-    contentSecurityPolicy: false,
-    crossOriginEmbedderPolicy: false,
-    crossOriginResourcePolicy: false,
-  }),
-  corsMiddleware,
-  xss(),
-  mongoSanitize(),
-  hpp(),
-  apiLimiter,
+	helmet(),
+	corsMiddleware,
+	xss(),
+	mongoSanitize(),
+	hpp(),
+	apiLimiter,
 ];
 
 module.exports = appMiddlewares;
